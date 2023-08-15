71c5d6a50fcffcf53c15171c8536f946944e6692
EC2 Default User <ec2-user@ip-172-31-24-154.ap-northeast-1.compute.internal>
2023-08-15 05:40:10 +0000

    heroku

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 #settings.pyからそのままコピー
-SECRET_KEY = '***************************'
+SECRET_KEY = '7b$o)ja+)q)0s(mn!ma3kgq+w#o1=qp5)_+5h!+p@g!g0t3xah'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
