2cb3520fc73fd4ed50ee309fda18cbd68370288a
EC2 Default User <ec2-user@ip-172-31-24-154.ap-northeast-1.compute.internal>
2023-08-16 15:35:56 +0000

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mysite.wsgi --log-file -
+web: gunicorn apl_name : app --log-file -
--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -9,8 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', #Herokuのデータベースに関する設定
-        
-        #'ENGINE': 'django.db.backends.mysql', #mysqlをデータベースとして使用することを指定
         'NAME': 'mysite', #データベース名
         'USER': 'DBuser', #データベースを作成したユーザー名
         'PASSWORD': 'DBuser-24', #ログインするためのパスワード
